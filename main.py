7d483ebe3b49f09689273e55e054582ca0d1169f
Jahir Raihan Joy <raihan.joy10@gmail.com>
2024-04-07 01:44:08 +0600

    Removed dir codes

--- main.py
+++ main.py
@@ -18,32 +18,3 @@ def infect(files):
 
 
 infect(os.listdir('project_files'))
-
-
-# paths = os.listdir("/")
-# print(paths)
-# for path in paths:
-#     try:
-#         print(os.listdir("/"+path))
-#     except:
-#         pass
-#
-#
-# def recursive_files(prefix, paths):
-#     for path in paths:
-#         try:
-#
-#             if prefix == '/':
-#                 path = prefix + path
-#             else:
-#                 path = prefix + '/' + path
-#
-#             lst_paths = os.listdir(path)
-#             print("Path : ", path, '=', lst_paths)
-#             print()
-#             recursive_files(path, lst_paths)
-#         except:
-#             return
-
-
-# recursive_files('', [''])
